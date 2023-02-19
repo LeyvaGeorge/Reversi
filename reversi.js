371fb5d4df0fb8fcd009435c07ec842bac5ddfa3
George Leyva <leyvageorge@hotmail.com>
2023-02-19 11:24:22 -0800

    added color changer to game

--- reversi.js
+++ reversi.js
@@ -10,32 +10,50 @@
 //     }    
 // }
 
+let turn = 'black';
 
-
-class tileInfo {
+class tileInfo {    //Start of class ********************* 
     constructor(Name,Tile){
         this.name = Name
         this.tile = Tile;
         
     }
-    log() {
+    logg() {
         console.log(this.name)
+        console.log(this.tile)
     }
     changeColor() {
 
         let divName = document.getElementById(`${this.name}`);
         divName.addEventListener("click", () =>{
-            divName.style.background = "blue";
+            if(this.tile == false){
+                if(turn == 'black'){
+                    divName.style.background = "black";
+                    turn = 'white'
+                } else {
+                    divName.style.background = "white";
+                    turn = 'black'
+                }
+                    
+            } else {
+                console.log("Tile is already contains a peice")
+            }
+            
+            this.tile = true;
+            
+            this.logg()
         });
-        //
+        
     }   
 }
 let testArray = ['A1','A2','A3','A4','A5','A6','A7','A8']
 for(let i = 0; i < testArray.length; i++){
-    //TODO
+    const Information = new tileInfo(testArray[i],false);//Constructor to class*****************
+    Information.changeColor();
+    
+    
 }
-const Information = new tileInfo('A1',false);
-Information.changeColor();
+
 
 // for(let i = 0; i < row.length; i++) {
 //     for(let j = 0; j < column.length; j++) {
