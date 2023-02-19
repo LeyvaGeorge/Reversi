011aa5db5b82d9936dc8baee0821f89609f79c86
George Leyva <leyvageorge@hotmail.com>
2023-02-19 11:57:55 -0800

    changes to who's turn is it

--- reversi.js
+++ reversi.js
@@ -11,6 +11,7 @@
 // }
 
 let turn = 'black';
+let playersTurn = document.getElementById('player')
 
 class tileInfo {    //Start of class ********************* 
     constructor(Name,Tile){
@@ -22,6 +23,12 @@ class tileInfo {    //Start of class *********************
         console.log(this.name)
         console.log(this.tile)
     }
+    plyrTurn(color) {
+        if(color == 'black')
+            playersTurn.innerHTML = 'Black'
+        else
+            playersTurn.innerHTML = 'White'    
+    }
     changeColor() {
 
         let divName = document.getElementById(`${this.name}`);
@@ -30,28 +37,28 @@ class tileInfo {    //Start of class *********************
                 if(turn == 'black'){
                     divName.style.background = "black";
                     turn = 'white'
+                    this.tile = true;
+                    this.plyrTurn(turn)
+                    console.log(turn)
                 } else {
                     divName.style.background = "white";
                     turn = 'black'
-                }
-                    
+                    this.tile = true;
+                    this.plyrTurn(turn)
+                    console.log(turn)
+                }          
             } else {
                 console.log("Tile is already contains a peice")
             }
-            
-            this.tile = true;
-            
-            this.logg()
-        });
-        
+            //this.logg()
+        });  
     }   
 }
+//Creates the Array of the first row to test
 let testArray = ['A1','A2','A3','A4','A5','A6','A7','A8']
 for(let i = 0; i < testArray.length; i++){
     const Information = new tileInfo(testArray[i],false);//Constructor to class*****************
     Information.changeColor();
-    
-    
 }
 
 
@@ -60,4 +67,14 @@ for(let i = 0; i < testArray.length; i++){
 //         let docUItem = document.getElementById(row[i] + column[j])
 //         docUItem.classList.add(tileInfo)
 //     }    
+// }
+
+//         Changing the footers see who's turn is it
+
+playersTurn.innerHTML = 'Black' 
+
+
+// if (turn == 'black'){
+//    } else {
+//     playersTurn.innerHTML = 'White'
 // }
