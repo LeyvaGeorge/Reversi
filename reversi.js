ea53328d22bce4b36dc816a63e872347d9c3d329
George Leyva <leyvageorge@hotmail.com>
2023-02-19 12:54:33 -0800

    added changes to score as well as increased the grid to all rows

--- reversi.js
+++ reversi.js
@@ -11,6 +11,10 @@
 // }
 
 let turn = 'black';
+let blk = 0;
+let wht = 0;
+let blackScore = document.getElementById('blkScore')
+let whiteScore = document.getElementById('whtScore')
 let playersTurn = document.getElementById('player')
 
 class tileInfo {    //Start of class ********************* 
@@ -19,15 +23,27 @@ class tileInfo {    //Start of class *********************
         this.tile = Tile;
         
     }
-    logg() {
+    logg() {        //console logs the infomation selected
         console.log(this.name)
         console.log(this.tile)
     }
-    plyrTurn(color) {
+    plyrTurn(color) { // Changes the text in the footer to reflect who's turn it is.
         if(color == 'black')
             playersTurn.innerHTML = 'Black'
         else
             playersTurn.innerHTML = 'White'    
+    }
+    cngAddBlkScore( ){
+        blk++;
+        console.log(blk)
+        console.log(wht)
+        blackScore.innerHTML = `Black: ${blk}`
+    }
+    cngAddWhtScore() {
+        wht++;
+        console.log(blk)
+        console.log(wht)
+        whiteScore.innerHTML = `White: ${wht}`
     }
     changeColor() {
 
@@ -38,14 +54,15 @@ class tileInfo {    //Start of class *********************
                     divName.style.background = "black";
                     turn = 'white'
                     this.tile = true;
-                    this.plyrTurn(turn)
-                    console.log(turn)
+                    this.plyrTurn(turn);
+                    this.cngAddBlkScore();
                 } else {
                     divName.style.background = "white";
                     turn = 'black'
                     this.tile = true;
-                    this.plyrTurn(turn)
-                    console.log(turn)
+                    this.plyrTurn(turn);
+                    this.cngAddWhtScore();
+                    
                 }          
             } else {
                 console.log("Tile is already contains a peice")
@@ -55,19 +72,23 @@ class tileInfo {    //Start of class *********************
     }   
 }
 //Creates the Array of the first row to test
-let testArray = ['A1','A2','A3','A4','A5','A6','A7','A8']
+/*let testArray = ['A1','A2','A3','A4','A5','A6','A7','A8']
 for(let i = 0; i < testArray.length; i++){
     const Information = new tileInfo(testArray[i],false);//Constructor to class*****************
     Information.changeColor();
-}
+}*/
 
 
-// for(let i = 0; i < row.length; i++) {
-//     for(let j = 0; j < column.length; j++) {
-//         let docUItem = document.getElementById(row[i] + column[j])
-//         docUItem.classList.add(tileInfo)
-//     }    
-// }
+ let row = ['A','B','C','D','E','F','G','H']
+ let column = ['1','2','3','4','5','6','7','8']
+
+for(let i = 0; i < row.length; i++) {
+    for(let j = 0; j < column.length; j++) {
+        const Information = new tileInfo(row[i] + column[j],false);//Constructor to class*****************
+        Information.changeColor();
+    }
+}    
+
 
 //         Changing the footers see who's turn is it
 
