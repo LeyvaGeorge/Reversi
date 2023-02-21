9b6745886176053e215f693fb9ca0f5b1abac131
George Leyva <leyvageorge@hotmail.com>
2023-02-21 15:39:13 -0800

    refactord all the code to previous state of OLD Code

--- reversi.js
+++ reversi.js
@@ -4,7 +4,8 @@ let blackScore = document.getElementById('blkScore');
 let whiteScore = document.getElementById('whtScore');
 let playersTurn = document.getElementById('player');
 
-class tile {
+//Tile is simple what the tile box contain
+class tile { //=======================================================
     constructor(Name,Fill,Color) {
         this.name = Name;   //string. The name of tile
         this.fill = Fill;   // T/F  if tile is occupied
@@ -12,14 +13,16 @@ class tile {
         
     }
     addPiece(){
-        this.fill = T;
+        this.fill = true;
     }
     cngColor(color){
+        this.color = color
         document.getElementById(this.name).style.background = color;
     } 
 }
 
-class gameSystem extends tile{
+//Game class to add functionality to the game
+class gameSystem extends tile{  //=====================================
     constructor(plrColor) {
         super();
         this.boxItems = {};
@@ -28,18 +31,23 @@ class gameSystem extends tile{
         this.trnColor = plrColor //the players selects the Color
         //strtGame();
     }
+    
+    //Sets the Game 
     strtGame() {
         this.fillArray();
         this.scoreBlk = 0;
         this.scoreWht = 0;
+        this.listen();
+        playersTurn.innerHTML = `${this.addPiece.trnColor}'s Color`
     }
+
     fillArray() {
         for(let i = 0; i < rowL.length; i++) {
             for(let j = 0; j < columnN.length; j++) {
                 let letNum = rowL[i] + columnN[j];
                 if( letNum == "D4" || letNum == "E5"){
                     this.boxItems[letNum] = new tile(letNum,true,""); //Constructor to fill black tiles
-                    this.boxItems[letNum].cngColor("black")
+                    this.boxItems[letNum].cngColor("black");
                 }
                 else if(letNum == "D5" || letNum == "E4") {
                     this.boxItems[letNum] = new tile(letNum,true,""); //constructor to fill white tiles
@@ -47,18 +55,70 @@ class gameSystem extends tile{
                 }
                 else{
                     this.boxItems[letNum] = new tile(letNum,false,"");     //Constructor to class empty
-                }
-                
+                } 
             }
-        }        
+        }
+        
+        this.altrpoints();        
     }
-    chngColor(){
+    
+    //This function checks all the collumns and rows to see if collor on blocks has changed and updates the board
+    altrpoints(){ 
+        let white = 0;
+        let black = 0;
+        
+        for(let i = 0; i < rowL.length; i++) {
+            for(let j = 0; j < columnN.length; j++) {
+                let letNum = rowL[i] + columnN[j];
+                if(this.boxItems[letNum].color == "black"){
+                    black++;  
+                }else if(this.boxItems[letNum].color == "white"){
+                    white++;
+                }else{}
+            } 
+        }
+        blackScore.innerHTML =`Black: ${black}`
+        whiteScore.innerHTML = `White: ${white}`
+    }
+
+    //Adds EventListener to the array
+    listen() {
         for(let i = 0; i < rowL.length; i++) {
             for(let j = 0; j < columnN.length; j++) {
                 let letNum = rowL[i] + columnN[j];
                 let divName = document.getElementById(`${letNum}`);
+                divName.addEventListener("click", () =>{
+                    //Alter Code
+                    
+                    if(this.boxItems[`${letNum}`].fill == false){
+                        if(this.trnColor == 'black'){
+                            this.boxItems[`${letNum}`].cngColor("black");    //changes the background to black
+                            this.boxItems[`${letNum}`].addPiece();               //changes it the tile to True
+                            this.chngTurn();                                //updates the color to white->black of vice versa                        
+                            //this.tile = true;
+                            //this.plyrTurn(turn);
+                            //this.cngAddBlkScore();
+                        } else {
+                            this.boxItems[`${letNum}`].cngColor("white")    //changes the background to black
+                            this.boxItems[`${letNum}`].addPiece()               //changes it the tile to T
+                            this.chngTurn(); 
+                            
+                        }          
+                    } else {
+                        alert("Tile is already contains a peice")
+                    }
+                })
             }
-        }    
+        }        
+    }
+    chngTurn() {
+        if(this.trnColor == 'black'){
+            this.trnColor = 'white';
+            playersTurn.innerHTML = "White's Turn"
+        }else{
+            this.trnColor = 'black';
+            playersTurn.innerHTML = "Black's Turn"
+        }
     }
     //Function(){
     //}   
@@ -67,7 +127,7 @@ class gameSystem extends tile{
 let GAME = new gameSystem("black");
 GAME.strtGame();
 
-// ******==========********    OLD CODE ********==========******
+// ******==========********    OLD CODE    ********==========******
 
 
 // let row = ['A','B','C','D','E','F','G','H']
@@ -115,30 +175,30 @@ GAME.strtGame();
 //         console.log(wht)
 //         whiteScore.innerHTML = `White: ${wht}`
 //     }
-//     changeColor() {
-//         let divName = document.getElementById(`${this.name}`);
-//         divName.addEventListener("click", () =>{
-//             if(this.tile == false){
-//                 if(turn == 'black'){
-//                     divName.style.background = "black";
-//                     turn = 'white'
-//                     this.tile = true;
-//                     this.plyrTurn(turn);
-//                     this.cngAddBlkScore();
-//                 } else {
-//                     divName.style.background = "white";
-//                     turn = 'black'
-//                     this.tile = true;
-//                     this.plyrTurn(turn);
-//                     this.cngAddWhtScore();
+    // changeColor() {
+    //     let divName = document.getElementById(`${this.name}`);
+    //     divName.addEventListener("click", () =>{
+    //         if(this.tile == false){
+    //             if(turn == 'black'){
+    //                 divName.style.background = "black";
+    //                 turn = 'white'
+    //                 this.tile = true;
+    //                 this.plyrTurn(turn);
+    //                 this.cngAddBlkScore();
+    //             } else {
+    //                 divName.style.background = "white";
+    //                 turn = 'black'
+    //                 this.tile = true;
+    //                 this.plyrTurn(turn);
+    //                 this.cngAddWhtScore();
                     
-//                 }          
-//             } else {
-//                 console.log("Tile is already contains a peice")
-//             }
-//             //this.logg()
-//         });  
-//     }   
+    //             }          
+    //         } else {
+    //             console.log("Tile is already contains a peice")
+    //         }
+    //         //this.logg()
+    //     });  
+    // }   
 // }
 // //Creates the Array of the first row to test
 // /*let testArray = ['A1','A2','A3','A4','A5','A6','A7','A8']
